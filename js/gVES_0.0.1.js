57dc00884ccaf000ccacf0a32a9d520e860dc076
william chiu <william.chiu16@gmail.com>
2018-04-12 01:22:26 -0400

    name file calendar.ics

--- js/gVES_0.0.1.js
+++ js/gVES_0.0.1.js
@@ -16317,7 +16317,15 @@ breakpointApp.directive('breakpoint', ['$window', '$rootScope', function($window
             if (settings.debug) {
                 console.log(icsMSG);
             }
-            window.location.href = ("data:text/calendar;charset=utf8," + escape(icsMSG));
+            saveContent(("data:text/calendar;charset=utf8," + escape(icsMSG)), "calendar.ics");
+
+            function saveContent(fileContents, fileName)
+            {
+                var link = document.createElement('a');
+                link.download = fileName;
+                link.href = 'data:,' + fileContents;
+                link.click();
+            }
         }
         return this.each(function() {
             var elem = $(this);
